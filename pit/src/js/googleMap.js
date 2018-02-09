d7ab5ebcf2c8d3c2912d15c66ff35241d1948bc8
maksdk <havryshchenko@gmail.com>
2018-02-09 23:19:20 +0200

    change full screen google maps

--- pit/src/js/googleMap.js
+++ pit/src/js/googleMap.js
@@ -1,4 +1,5 @@
 function initMap(){
+	
 	var kyiv,
 		map,
 		marker,
@@ -8,12 +9,14 @@ function initMap(){
 		navigator.geolocation.getCurrentPosition(positionReceived,positionNotReceived);
 	}
 	function ZoomControl(controlDiv, map) {
-	   	controlDiv.style.padding = '5px';
+	   	controlDiv.style.padding = '10px';
 		var zoomIn = document.createElement('div');
 	   		zoomIn.classList.add("button");
+	   		zoomIn.classList.add("buttonIn");
 	   		controlDiv.appendChild(zoomIn);
 	   	var zoomOut = document.createElement('div');
 	   		zoomOut.classList.add("button");
+	   		zoomOut.classList.add("buttonOut");
 	   		controlDiv.appendChild(zoomOut);
 		google.maps.event.addDomListener(zoomIn, 'click', function() {   
 	   		map.setZoom(map.getZoom() + 1);
@@ -22,20 +25,7 @@ function initMap(){
 	     	map.setZoom(map.getZoom() - 1);
 	    });  
 	}
-	function FullScreen(fullScreenDiv, map, divMap){
-	 	fullScreenDiv.style.padding = '5px';
-	 	var fullScreen = document.createElement('div');
-	   		fullScreen.classList.add("button");
-	   		fullScreenDiv.appendChild(fullScreen);
-		google.maps.event.addDomListener(fullScreen, 'click', function(e) {
-	   		var height = document.documentElement.clientHeight;  
-			if(divMap.style.height == false){
-	   			divMap.style.height = height +"px";
-	   		} else {
-	   			divMap.style.height ="";
-	   		}
-	   	});
-	}
+	
 	function toggleBounce() {
 	  if (marker.getAnimation() !== null) {
 	    marker.setAnimation(null);
@@ -64,23 +54,25 @@ function initMap(){
 	
 	function initialize() {
  		var divMap = document.getElementById('map');
- 	
  		kyiv = {lat:50.450104, lng:30.524135};
  		map = new google.maps.Map(divMap, {
    	 		zoom: 18,
      		center: kyiv,
      		disableDefaultUI: true,
+     		fullscreenControl: true,
      		styles: [ {"stylers": [{"hue": "#baf4c4"},{"saturation": 10}]},{"featureType": "water","stylers": [{"color": "#effefd"}]}]
    		 });
-	    //map.addListener('click', (e) => {
-		// 	addMarker({lat: e.latLng.lat(), lng:e.latLng.lng()});
-		// });
-		var zoomDiv = document.createElement('div'),
+ 		var zoomDiv = document.createElement('div'),
     		zoomControl = new ZoomControl(zoomDiv, map);
     		map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(zoomDiv);
-
-   		var fullScreenDiv = document.createElement('div'),
-    	    fullScreen = new FullScreen(fullScreenDiv, map, divMap);
-    		map.controls[google.maps.ControlPosition.TOP_RIGHT].push(fullScreenDiv);
 	}
 }
+
+window.addEventListener('load', function () {
+var img = document.getElementsByClassName('gm-fullscreen-control');
+	img[0].setAttribute("src", "../img/map/fullscreen_red.png");
+	img[0].setAttribute("style", "heigth: 24px; width:24px;");
+});
+
+
+
