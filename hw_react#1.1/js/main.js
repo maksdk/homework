f9f525cf796bf00fb2d79e5de66b1c535b8766a2
maksdk <havryshchenko@gmail.com>
2018-03-09 16:10:28 +0200

    added hw

---
+++ hw_react#1.1/js/main.js
@@ -0,0 +1,69 @@
+class Buttons extends React.Component {
+	state = {
+		mainColor: "#004080"
+	}
+	clickButton = (e)  => {
+		let parent = e.target.parentNode;
+		parent.classList.add("active");
+		
+		setTimeout(function() {
+			parent.classList.remove("active");
+			parent.classList.add("finished");
+		}, 2000);
+		
+		setTimeout(function() {
+			parent.classList.remove("finished");
+		}, 4000);
+	}
+	watchColorPicker = (e) => {
+		let getColor = e.target.value;
+		this.setState(({color}) => ({
+			mainColor: getColor
+	    }))
+	}
+	render(){
+		return (
+			<div className="container">
+				<label
+					className="label"
+				>
+					Choose color:
+					<input
+						className="inputColor"
+						onChange={this.watchColorPicker}
+						type="color"
+						value={`${this.state.mainColor}`}
+					/>
+				</label>
+				<button 
+					className="button"
+					style={{backgroundColor:`${this.state.mainColor}`}}
+				>
+				<span 
+					className="button__submit"
+					onClick={this.clickButton}
+				>
+					click me
+				</span>
+				<span 
+					className="button__progress"
+				>
+				</span>
+				<span 
+					className="button__loading"
+				>
+				</span>
+				<span 
+					className="button__checked"
+				>
+				</span>
+				</button>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+  <Buttons />,
+  document.body
+)
