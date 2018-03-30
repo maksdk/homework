33d3984979ae089a6877fae5f4e6ae1018a16178
maksdk <havryshchenko@gmail.com>
2018-03-30 17:53:14 +0300

    add hw http request

---
+++ React/5.Http_request/src/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import {
+	createStore,
+	applyMiddleware,
+	combineReducers,
+} from "redux";
+
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+
+import App from "./App";
+
+function users(state = [], action) {
+	if (action.type === "Loading_user") {
+		return [...state, action.payload];
+	}
+	return state;
+}
+
+const reducer = combineReducers({
+	users,
+});
+
+const store = createStore(
+	reducer,
+	composeWithDevTools(applyMiddleware(thunk)),
+);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root"),
+);
