b7c8b6665ff1185382f5bd298b0150322a60e5ab
maksdk <havryshchenko@gmail.com>
2018-03-30 08:59:08 +0300

    refresh react hw

--- hw_react#2.2/todo/src/redux/reducers/addTask.js
+++
@@ -1,8 +0,0 @@
-export default function addTask (state = [], action) {
-  if (action.type === 'Add_task') {
-  	return [...state, action.payload];
-  } else if (action.type === 'Delete_task') {
-  	console.log("====DELETE TASK====");
-  }
-  return state;
-}
--- hw_react#2.2/todo/src/redux/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from 'redux';
-
-import addTask from './addTask.js'
-
-export default combineReducers({
-	addTask
-});
