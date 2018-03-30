2787bdb92e46951d937913a777fbb8a60ba1901b
maksdk <havryshchenko@gmail.com>
2018-03-30 16:49:42 +0300

    add hw

--- React/4. React_router/src/index.js
+++ React/4. React_router/src/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import {
+	createStore,
+	applyMiddleware,
+	combineReducers,
+} from "redux";
+
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 
 import App from './App';
 
+
+function users(state = [], action) {
+	if (action.type === "Loading_user") {
+		return [...state, action.payload];
+	}
+	return state;
+}
+
+const store = createStore(
+	users,
+  	composeWithDevTools(applyMiddleware(thunk))
+);
+
 ReactDOM.render(
-	<App />, document.getElementById('root')
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root"),
 );
