a8d3807ca89dcfeef70ee00a393ed278204ef233
maksdk <havryshchenko@gmail.com>
2018-12-16 12:09:55 +0200

    sidebar was corrected

--- components/src/sidebar/Sidebar.js
+++ components/src/sidebar/Sidebar.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
+import './style.css';
 
-let items = new Array(10);
+let items = ['Item', 'Item', 'Item', 'Item', 'Item'];
 
 export default class Sidebar extends Component{
 	constructor(props){
@@ -8,32 +9,82 @@ export default class Sidebar extends Component{
 		this.state = {
 			isShowed:false
 		};
+		this.wasDowned = false;
+		this.startX = null;
+		this.minX = 5;
+		this.sidebar = React.createRef();
 	}
-	toggleVisible(e){
-		e.preventDefault();
+	toggleVisible(){
+		let {isShowed} =this.state;
+		this.setState({isShowed:!isShowed})
+	}
+	hide(e){
+		if(e){
+			e.preventDefault();
+			e.stopPropagation();
+		}
+		this.wasDowned = false;
+		this.startX = null;
+		this.sidebar.current.style.left = 0;
+		this.setState({isShowed:false});
+	}
+	onDownSidebar(e){
 		e.stopPropagation();
-		
-		let {isShowed} = this.state;
-		this.setState({isShowed:!isShowed});
+		this.wasDowned = true;
+		this.startX = e.clientX;
+
+		document.addEventListener('mousemove',(e) => this.onMoveSidebar(e), false);
 	}
+	onUpSidebar(e){
+		e.stopPropagation();
+		this.wasDowned = false;
+		this.startX = null;
+		
+		document.removeEventListener('mousemove',() => this.onMoveSidebar());
+	}
+	onMoveSidebar(e){
+		e.stopPropagation();
+		if(!this.wasDowned) return;
+
+		let delta = this.startX - e.clientX;
+		if(delta > 10 ){
+			this.sidebar.current.style.left = -delta + 'px';
+		} else {
+			this.sidebar.current.style.left = 0;
+		}
+
+		if(delta >= 150 || e.clientX <= this.minX) {
+			this.hide()
+		} 
+	}
+
+
 	render(){
 		let {isShowed} = this.state;
 		return(
-			<div className='sidebar'>
-				<div className={`sidebar_left ${isShowed ? 'sidebar_left_visible' : ''}`}>
+			<div 
+				className='sidebar'
+				onMouseDown={(e) => this.hide(e)}
+			>
+				<div 
+					className={`sidebar_left ${isShowed ? 'sidebar_left_visible' : ''}`}
+					onMouseDown={(e) => this.onDownSidebar(e)}
+					onMouseUp={(e) => this.onUpSidebar(e)}
+					ref={this.sidebar}
+				>
 					<div 
 						onClick={(e) => this.toggleVisible(e)}
 						children='X'
 						className='sidebar_closeBtn'
 					/>
-					<div className='sidebar_title'></div>
 					{items && items.map((item, i) => {
-							return(
-								<div
-									className='sidebar_item'
-									children={`Item_${i}`}
-								/>
-							)
+						return(
+							<div
+								key={i}
+								className='sidebar_item'
+								children={`${item}_${i}`}
+							/>
+						)
 					})}
 				</div>
 			</div>	
